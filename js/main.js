dde721f6d7ed88dd12565f495ed7cf2bab9572e2
Jason ONeill <jason.a.oneill@gmail.com>
2024-02-08 13:02:55 +0000

    added different color bg for image containers on light/dark switch

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const checkbox = document.getElementById('colorCheckbox');
 const svgs = document.querySelectorAll('svg');
+const imageContainers = document.querySelectorAll('.image-container'); // Updated selector
 
 /* rgb(250, 247, 232) or rgb(250, 248, 239) = White
    rgb(17, 16, 16) = Black */
@@ -11,11 +12,18 @@ checkbox.addEventListener('change', function() {
         svgs.forEach(svg => {
             svg.style.fill = 'rgb(17, 16, 16)';
         });
+        imageContainers.forEach(container => {
+            container.style.backgroundColor = 'rgb(194, 194, 194)';
+        });
     } else {
         document.body.style.backgroundColor = 'rgb(17, 16, 16)';
         document.body.style.color = 'rgb(250, 247, 232)';
         svgs.forEach(svg => {
             svg.style.fill = 'rgb(250, 247, 232)';
         });
+        imageContainers.forEach(container => {
+            container.style.backgroundColor = '#1d1d1d';
+        });
     }
 });
+
