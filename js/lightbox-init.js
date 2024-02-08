530c3b442b3273392b43209e58d462652543b95f
Jason ONeill <jason.a.oneill@gmail.com>
2024-02-08 11:41:57 +0000

    added container to light button to center text

--- js/lightbox-init.js
+++ js/lightbox-init.js
@@ -3,8 +3,10 @@ import Lightbox from './photoswipe-lightbox.esm.js';
 const lightbox = new Lightbox({
   gallery: '.image-container',
   children: '.screenshot',
+
   pswpModule: () => import('./photoswipe.esm.js')
 });
 
-lightbox.init();
+lightbox.init(); 
+
 
