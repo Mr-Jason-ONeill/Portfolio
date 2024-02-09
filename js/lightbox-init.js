b994cada4d1766902383df8e036d3de891eac3bc
Jason ONeill <jason.a.oneill@gmail.com>
2024-02-09 10:46:17 +0000

    trying to get js file to work

--- js/lightbox-init.js
+++ js/lightbox-init.js
@@ -1,39 +1,17 @@
 
 import PhotoSwipeLightbox from '/photoswipe/photoswipe-lightbox.esm.js';
+
+
 const lightbox = new PhotoSwipeLightbox({
+
   gallery: '.project-image-wrapper',
   children: 'a',
   showHideAnimationType: 'fade',
   pswpModule: () => import('/photoswipe/photoswipe.esm.js')
 });
-lightbox.init();
-
-
-
-/* import PhotoSwipeLightbox from '/photoswipe/photoswipe-lightbox.esm.js';
-
-const options = {
-  gallery: '.project-image-wrapper',
-  children: 'a',
-
-  secondaryZoomLevel: 'fit',
-  controls: true,
-  counter: true,
-
-  imageClickAction: 'close',
-  tapAction: 'close',
-
-  // tap delay is removed if set to false
-  doubleTapAction: false,
-  
-  pswpModule: () => import('/photoswipe/photoswipe.esm.js')
-
-};
-
-const lightbox = new PhotoSwipeLightbox(options);
 
 lightbox.init();
-*/
+
 
 
 
