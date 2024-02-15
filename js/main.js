50a63b9e53fef50076e7e65d25a4bb78a2d035cc
Jason ONeill <jason.a.oneill@gmail.com>
2024-02-15 09:09:11 +0000

    changed font sizes

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const imageContainers = document.querySelectorAll('.image-container');
 
 checkbox.addEventListener('change', function() {
     if (this.checked) {
-        document.body.style.backgroundColor = 'rgb(250, 248, 239)';
+        document.body.style.backgroundColor = 'rgb(230 230 230)';
         document.body.style.color = 'rgb(17, 16, 16)';
         svgs.forEach(svg => {
             svg.style.fill = 'rgb(17, 16, 16)';
@@ -17,9 +17,9 @@ checkbox.addEventListener('change', function() {
         });
     } else {
         document.body.style.backgroundColor = 'rgb(17, 16, 16)';
-        document.body.style.color = 'rgb(250, 247, 232)';
+        document.body.style.color = 'rgb(230 230 230)';
         svgs.forEach(svg => {
-            svg.style.fill = 'rgb(250, 247, 232)';
+            svg.style.fill = 'rgb(230 230 230)';
         });
         imageContainers.forEach(container => {
             container.style.backgroundColor = '#1d1d1d';
